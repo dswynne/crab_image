8aac6859366df819d32dbac036139e9db01aafee
David.Wynne <dswynne16@gmail.com>
2025-04-16 19:54:30 -0400

    added power law equalizer

--- src/equalize.rs
+++ src/equalize.rs
@@ -11,4 +11,14 @@ pub fn logeq(mut arr: [[f32; 10]; 10], c: f32) -> [[f32; 10]; 10] {
     return ret;
 }
 
-// fn power_law() {}
+pub fn powerlaweq(mut arr: [[f32; 10]; 10], c: f32, g: f32) -> [[f32; 10]; 10] {
+    let mut ret: [[f32; 10]; 10] = [[0.0; 10]; 10];
+
+    for (i, row) in arr.iter_mut().enumerate() {
+        for (y, col) in row.iter_mut().enumerate() {
+            ret[i][y] = c * col.powf(g);
+        }
+    }
+
+    return ret;
+}
