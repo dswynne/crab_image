e8280c8f828f43290fd57ebe09ce5fb56ab41b85
David.Wynne <dswynne16@gmail.com>
2025-04-19 16:05:51 -0400

    adding image crate, didn't get it working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+// Local
 mod equalize;
+
+// External
+use image::ImageReader;
 
 fn main() {
     // Setup
     // TODO: This should be an image at some point
     let grid: [[f32; 10]; 10] = [[5.0; 10]; 10];
+    // TODO: I am confused on why this library no work
+    // let img = ImageReader::open("data/lena.tif")?.decode()?;
     let c: f32 = 1.0;
 
     // Run log equalization
