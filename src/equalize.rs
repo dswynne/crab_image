24f2bef407f738aded84d05c7bf08a931188ca0a
David Wynne <dswynne16@gmail.com>
2025-08-01 20:27:47 -0400

    moving to using Array2 for all current equalization methods

--- src/equalize.rs
+++ src/equalize.rs
@@ -1,24 +1,17 @@
-// TODO: Can I make this accept variable sized inputs?
-pub fn logeq(mut arr: [[f32; 10]; 10], c: f32) -> [[f32; 10]; 10] {
-    let mut ret: [[f32; 10]; 10] = [[0.0; 10]; 10];
+use ndarray::Array2;
 
-    for (i, row) in arr.iter_mut().enumerate() {
-        for (y, col) in row.iter_mut().enumerate() {
-            ret[i][y] = c * (*col - 1.0).log10();
-        }
+pub fn logeq(data: &Array2<f32>, c: f32) -> Array2<f32> {
+    let mut ret = data.clone();
+    for ((i, j), value) in data.indexed_iter() {
+        ret[[i, j]] = c * (value - 1.0).log10();
     }
-
     return ret;
 }
 
-pub fn powerlaweq(mut arr: [[f32; 10]; 10], c: f32, g: f32) -> [[f32; 10]; 10] {
-    let mut ret: [[f32; 10]; 10] = [[0.0; 10]; 10];
-
-    for (i, row) in arr.iter_mut().enumerate() {
-        for (y, col) in row.iter_mut().enumerate() {
-            ret[i][y] = c * col.powf(g);
-        }
+pub fn powerlaweq(data: &Array2<f32>, c: f32, g: f32) -> Array2<f32> {
+    let mut ret = data.clone();
+    for ((i, j), value) in data.indexed_iter() {
+        ret[[i, j]] = c * value.powf(g);
     }
-
     return ret;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,26 @@ use std::f64::consts::PI;
 mod equalize;
 
 // External
-use image::ImageReader; // TODO: I am confused on why this library no work
-use ndarray::{AsArray, arr2};
+use ndarray::Array2;
+
 
 fn main() {
     // Setup
-    // TODO: This should be an image at some point
-    let grid: [[f32; 10]; 10] = [[5.0; 10]; 10];
-    // let test = AsArray(grid);
-
-    // let a = arr2(&[[1, 2, 3], [4, 5, 6]]);
-
-    // for ((x, y), value) in a.indexed_iter() {
-    //     print!("{x},{y},{value}|");
-    // }
-    let c: f32 = 1.0;
+    let mut grid = Array2::<f32>::ones((10, 10));
+    grid *= 10.0; // Scale the grid
 
     // Run log equalization
-    let mut logged: [[f32; 10]; 10] = equalize::logeq(grid, c);
+    let c: f32 = 1.0;
+    let logged = equalize::logeq(&grid, c);
 
     // Print results
-    for (_i, row) in logged.iter_mut().enumerate() {
-        for (_j, col) in row.iter_mut().enumerate() {
-            print!("{},", col);
+    print_array2(&logged);
+}
+
+fn print_array2(data: &Array2<f32>) {
+    for row in data.rows() {
+        for val in row {
+            print!("{}, ", val);
         }
         println!();
     }
